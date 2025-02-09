67104b414ab554c5bd8562cf2e7b9d236eaaf273
Naki <leandroreisubi@gmail.com>
2025-02-09 14:59:42 -0300

    Adiciona evento MouseDown aos TextBlocks
    As linhas de `<TextBlock>` no arquivo `MainWindow.xaml` foram modificadas para incluir o evento `MouseDown`, que chama o método `TextBlock_MouseDown` quando o usuário clica em um dos blocos de texto.
    
    No arquivo `MainWindow.xaml.cs`, foram adicionadas três novas variáveis e um novo método:
    - `TextBlock lastTextBlockClicked;` - uma variável para armazenar o último bloco de texto clicado.
    - `bool findingMatch = false;` - uma variável booleana para indicar se o programa está procurando uma correspondência.
    - `private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)` - um método que lida com o evento de clique do mouse em um bloco de texto. Este método oculta o bloco de texto clicado e verifica se ele corresponde ao último bloco de texto clicado. Se houver uma correspondência, ambos os blocos de texto permanecem ocultos; caso contrário, o último bloco de texto clicado é tornado visível novamente.

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -47,5 +47,27 @@ namespace MatchGame
                 animalEmoji.RemoveAt(index);
             }
         }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock; if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
     }
 }
